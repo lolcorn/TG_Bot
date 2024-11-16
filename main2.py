572feb3811e8a43afccd767e42e0051f9ee17351
Sergey Vovk <lolcorn-19@yandex.ru>
2024-11-16 07:40:00 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,22 @@
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "7510254418:AAEWQEdXNFQvmBEtAq4bRVRBV0__gFzXpGk"
+bot = Bot (token = api)
+Dispatcher (bot, storage= MemoryStorage())
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью')
+
+@dp.message_handler()
+async def all_massage(message):
+    await message.answer('Введите команду /start, чтобы начать общение')
+
+
+
+if __name__ == "__main__":
+    executor.start_polling (dp, skip_updates=True)
