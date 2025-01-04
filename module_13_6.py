ce6cacb609c1165a785706b2b51e48bb31d3a1a4
Sergey Vovk <lolcorn-19@yandex.ru>
2025-01-04 21:19:13 +0700

    module_13_6.py

--- module_13_6.py
+++ module_13_6.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-api = "7510254418:AAEWQEdXNFQvmBEtAq4bRVRBV0__gFzXpGk"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
